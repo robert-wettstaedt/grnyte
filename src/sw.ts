1ebd1f4af4d38cc037363e498f07dd8809c4ccb2
Robert Wettstädt <robert@wettstaedt.dev>
2025-02-14 19:18:38 +0100

    fix: sw

--- src/sw.ts
+++ src/sw.ts
@@ -2,23 +2,10 @@
 /// <reference types="vite/client" />
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+
+import { precacheAndRoute } from 'workbox-precaching'
 
 declare let self: ServiceWorkerGlobalScope
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
-})
-
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
-
-// clean old assets
-cleanupOutdatedCaches()
-
-let allowlist: undefined | RegExp[]
-if (import.meta.env.DEV) allowlist = [/^\/$/]
-
-// to allow work offline
-registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }))
