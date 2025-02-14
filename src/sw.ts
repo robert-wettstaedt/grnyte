5869838a494b107cf47272c19131fb8af73b3191
Robert Wettstädt <robert@wettstaedt.dev>
2025-02-14 20:04:09 +0100

    fix: sw

--- src/sw.ts
+++ src/sw.ts
@@ -3,7 +3,8 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -18,3 +19,9 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 // clean old assets
 cleanupOutdatedCaches()
+
+let allowlist: undefined | RegExp[]
+if (import.meta.env.DEV) allowlist = [/^\/$/]
+
+// to allow work offline
+registerRoute(new NavigationRoute(createHandlerBoundToURL('/'), { allowlist }))
