50b8874c316647cb8e8784fc91de007cf79c0ce7
Robert Wettstädt <robert@wettstaedt.dev>
2025-02-14 19:25:18 +0100

    fix: sw

--- src/sw.ts
+++ src/sw.ts
@@ -3,9 +3,12 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 
-import { precacheAndRoute } from 'workbox-precaching'
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 
 declare let self: ServiceWorkerGlobalScope
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
+
+// clean old assets
+cleanupOutdatedCaches()
