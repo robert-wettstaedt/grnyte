1ae99434acba0c93556483e843e4462f4710bd7a
Robert Wettstädt <robert@wettstaedt.dev>
2025-02-16 09:30:45 +0100

    feat: map improvements

---
+++ src/routes/(app)/blocks/[id=integer]/preview-image/+server.ts
@@ -0,0 +1,39 @@
+import { READ_PERMISSION } from '$lib/auth'
+import { createDrizzleSupabaseClient } from '$lib/db/db.server'
+import * as schema from '$lib/db/schema'
+import { imagePreviewHandler, loadFiles } from '$lib/nextcloud/nextcloud.server'
+import { error } from '@sveltejs/kit'
+import { eq } from 'drizzle-orm'
+
+export async function GET(event) {
+  const { locals, params } = event
+
+  if (!locals.userPermissions?.includes(READ_PERMISSION)) {
+    return new Response(null, { status: 404 })
+  }
+
+  const rls = await createDrizzleSupabaseClient(locals.supabase)
+
+  return await rls(async (db) => {
+    const block = await db.query.blocks.findFirst({
+      where: eq(schema.blocks.id, Number(params.id)),
+      with: {
+        topos: {
+          with: {
+            file: true,
+          },
+        },
+      },
+    })
+
+    const topo = block?.topos.at(0)
+
+    if (topo?.file == null) {
+      error(404, 'Not found')
+    }
+
+    const [topoFile] = await loadFiles([topo.file])
+
+    return imagePreviewHandler(topoFile.path, event)
+  })
+}
