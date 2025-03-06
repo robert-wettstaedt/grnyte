2e1c448eed231debd6eca79789583173472c98c2
Robert Wettstädt <robert@wettstaedt.dev>
2025-03-06 17:01:32 +0100

    chore: upgrade tailwind

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import Layout, { type LayoutProps } from '$lib/components/Layout'
-  import '@fortawesome/fontawesome-free/css/all.css'
-  import '../../app.css'
 
   let { children }: LayoutProps = $props()
 </script>
