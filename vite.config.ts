934869e2abd891ee299341cf1ec540593447d980
Robert Wettstädt <robert@wettstaedt.dev>
2025-03-17 18:08:12 +0100

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -13,72 +13,72 @@ export default defineConfig({
     tailwindcss(),
     sveltekit(),
     svelteTesting(),
-    SvelteKitPWA({
-      srcDir: './src',
-      mode: 'development',
-      strategies: 'injectManifest',
-      filename: 'sw.ts',
-      scope: '/',
-      base: '/',
-      useCredentials: true,
-      selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
-      manifest: {
-        name: 'grnyte',
-        short_name: 'grnyte',
-        description: 'Secure boulder topo and session tracker.',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        theme_color: '#8439A4',
-        background_color: '#1E1E2E',
-        icons: [
-          {
-            sizes: '192x192',
-            src: '/android-chrome-192x192.png',
-            type: 'image/png',
-          },
-          {
-            sizes: '512x512',
-            src: '/android-chrome-512x512.png',
-            type: 'image/png',
-          },
-        ],
-        screenshots: [
-          {
-            form_factor: 'narrow',
-            label: 'The landing page',
-            sizes: '1024x2216',
-            src: '/screenshot-mobile.jpg',
-            type: 'image/jpg',
-          },
-          {
-            form_factor: 'wide',
-            label: 'The landing page',
-            sizes: '1638x1024',
-            src: '/screenshot-desktop.jpg',
-            type: 'image/jpg',
-          },
-        ],
-      },
-      pwaAssets: {
-        config: true,
-      },
-      injectManifest: {
-        globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
-      },
-      workbox: {
-        globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
-      },
-      devOptions: {
-        enabled: true,
-        suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
-        type: 'module',
-        navigateFallback: '/',
-      },
-      kit: {
-        includeVersionFile: true,
-      },
-    }),
+    // SvelteKitPWA({
+    //   srcDir: './src',
+    //   mode: 'development',
+    //   strategies: 'injectManifest',
+    //   filename: 'sw.ts',
+    //   scope: '/',
+    //   base: '/',
+    //   useCredentials: true,
+    //   selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+    //   manifest: {
+    //     name: 'grnyte',
+    //     short_name: 'grnyte',
+    //     description: 'Secure boulder topo and session tracker.',
+    //     start_url: '/',
+    //     scope: '/',
+    //     display: 'standalone',
+    //     theme_color: '#8439A4',
+    //     background_color: '#1E1E2E',
+    //     icons: [
+    //       {
+    //         sizes: '192x192',
+    //         src: '/android-chrome-192x192.png',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         sizes: '512x512',
+    //         src: '/android-chrome-512x512.png',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //     screenshots: [
+    //       {
+    //         form_factor: 'narrow',
+    //         label: 'The landing page',
+    //         sizes: '1024x2216',
+    //         src: '/screenshot-mobile.jpg',
+    //         type: 'image/jpg',
+    //       },
+    //       {
+    //         form_factor: 'wide',
+    //         label: 'The landing page',
+    //         sizes: '1638x1024',
+    //         src: '/screenshot-desktop.jpg',
+    //         type: 'image/jpg',
+    //       },
+    //     ],
+    //   },
+    //   pwaAssets: {
+    //     config: true,
+    //   },
+    //   injectManifest: {
+    //     globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+    //   },
+    //   workbox: {
+    //     globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //     suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+    //     type: 'module',
+    //     navigateFallback: '/',
+    //   },
+    //   kit: {
+    //     includeVersionFile: true,
+    //   },
+    // }),
   ],
   test: {
     environment: 'jsdom',
