6a4ba02195389004a4bd244404d79fa065e66fa6
Robert Wettstädt <robert@wettstaedt.dev>
2025-05-16 14:56:48 +0200

    fix: calc grade histogram with `userGrade`

--- src/lib/blocks.server.ts
+++ src/lib/blocks.server.ts
@@ -249,7 +249,9 @@ export const getStatsOfArea = <T extends InferResultType<'areas'>>(
   const routes = recursive(area as unknown as NestedArea)
 
   const gradesObj = routes.map((route) => ({
-    grade: grades.find((grade) => grade.id === route.gradeFk)?.[user?.userSettings?.gradingScale ?? 'FB'],
+    grade: grades.find((grade) => grade.id === (route.userGradeFk ?? route.gradeFk))?.[
+      user?.userSettings?.gradingScale ?? 'FB'
+    ],
   }))
 
   return {
