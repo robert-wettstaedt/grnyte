f0a022825a2d2252019fca371f7543f43601878f
Robert Wettstädt <robert@wettstaedt.dev>
2025-08-02 09:42:05 +0200

    revert: remove custom splash screen

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,7 @@
 import { authGuard, supabase } from '$lib/hooks/auth'
 import { logger } from '$lib/hooks/logger'
 import { rateLimit } from '$lib/hooks/rate-limit'
-import { splash } from '$lib/hooks/splash'
 import { type Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
 
-export const handle: Handle = sequence(splash, rateLimit, supabase, authGuard, logger)
+export const handle: Handle = sequence(rateLimit, supabase, authGuard, logger)
