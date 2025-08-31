c8c0bfd6f9fee7434f545ad540058893b964eab8
Robert Wettstädt <robert@wettstaedt.dev>
2025-08-31 09:04:32 +0200

    fix: move redirect logic from page load to page component

---
+++ src/routes/(app)/areas/[...slugs]/_/blocks/[blockSlug]/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  import { page } from '$app/state'
+
+  const { slugs, blockSlug } = page.params
+  const mergedPath = ['areas', slugs, '_', 'blocks', blockSlug].join('/')
+  goto('/' + mergedPath + '?tab=topo', { replaceState: true })
+</script>
--- src/routes/(app)/settings/regions/+page.ts
+++
@@ -1,5 +0,0 @@
-import { goto } from '$app/navigation'
-
-export const load = () => {
-  goto('/settings')
-}
