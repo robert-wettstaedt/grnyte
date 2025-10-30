a41da9bf653bdebfe8059c9c226412e63d6687c3
Robert Wettstädt <robert@wettstaedt.dev>
2025-10-30 19:47:30 +0100

    refactor: add debug log

--- src/routes/(app)/auth/reset-password/+page.server.ts
+++ src/routes/(app)/auth/reset-password/+page.server.ts
@@ -21,6 +21,9 @@ export const actions = {
     }
 
     const updateResponse = await locals.supabase.auth.updateUser({ password: values.password })
+
+    console.log('updated user:', updateResponse.data)
+
     if (updateResponse.error != null) {
       return fail(400, { error: updateResponse.error.message })
     }
