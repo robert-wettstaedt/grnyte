edd2a5e31eda9f5fa67e1877b118808af4ee4d04
Robert Wettstädt <robert@wettstaedt.dev>
2026-01-01 13:36:19 +0100

    fix: header bar

--- src/lib/components/Layout/components/HeaderBar/HeaderBar.svelte
+++ src/lib/components/Layout/components/HeaderBar/HeaderBar.svelte
@@ -7,17 +7,19 @@
 </script>
 
 <AppBar class="sticky top-0 z-50 p-2 shadow-xl">
-  <AppBar.Lead>
-    <a class="flex gap-2" href="/">
-      <img src={Logo} alt={PUBLIC_APPLICATION_NAME} width={32} height={32} />
+  <AppBar.Toolbar class="grid-cols-[1fr_auto]">
+    <AppBar.Lead>
+      <a class="flex gap-2" href="/">
+        <img src={Logo} alt={PUBLIC_APPLICATION_NAME} width={32} height={32} />
 
-      <strong class="text-xl">{PUBLIC_APPLICATION_NAME}</strong>
-    </a>
-  </AppBar.Lead>
+        <strong class="text-xl">{PUBLIC_APPLICATION_NAME}</strong>
+      </a>
+    </AppBar.Lead>
 
-  {#if page.data.session?.user == null}
-    <AppBar.Trail>
-      <a href="/auth" class="btn btn-sm preset-filled-primary-500"> Get Started </a>
-    </AppBar.Trail>
-  {/if}
+    {#if page.data.session?.user == null}
+      <AppBar.Trail>
+        <a href="/auth" class="btn btn-sm preset-filled-primary-500"> Get Started </a>
+      </AppBar.Trail>
+    {/if}
+  </AppBar.Toolbar>
 </AppBar>
