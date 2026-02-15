ee24ea2bce7b4d8e619c455dbc3c7edcc6f85e1a
Robert Wettstädt <robert@wettstaedt.dev>
2026-02-15 09:02:36 +0100

    feat: restrict anon access to _all_ tables

--- src/lib/db/scripts/setup-table-permissions.ts
+++ src/lib/db/scripts/setup-table-permissions.ts
@@ -1,42 +1,20 @@
-import { getTableName, sql } from 'drizzle-orm'
+import { getTableName, isTable, sql } from 'drizzle-orm'
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 import * as schema from '../schema'
 
-const tables = [
-  schema.activities,
-  schema.areas,
-  schema.ascents,
-  schema.blocks,
-  schema.bunnyStreams,
-  schema.favorites,
-  schema.files,
-  schema.firstAscensionists,
-  schema.geolocations,
-  schema.grades,
-  schema.pushSubscriptions,
-  schema.regionMembers,
-  schema.regions,
-  schema.rolePermissions,
-  schema.routeExternalResource27crags,
-  schema.routeExternalResource8a,
-  schema.routeExternalResources,
-  schema.routeExternalResourceTheCrag,
-  schema.routes,
-  schema.routesToFirstAscensionists,
-  schema.routesToTags,
-  schema.tags,
-  schema.topoRoutes,
-  schema.topos,
-  schema.userRoles,
-  schema.users,
-  schema.userSettings,
-]
+const tables = Object.values(schema).filter((item) => isTable(item))
 
 export const migrate = async (db: PostgresJsDatabase<typeof schema>) => {
+  Object.values(schema).map((item) => isTable(item))
+
   for (const table of tables) {
     const tableName = getTableName(table)
     await db.execute(sql.raw(`revoke all on table public."${tableName}" from anon, public;`))
   }
 
+  await db.execute(sql`
+    revoke all on table public.keyv from anon, public;
+    ALTER TABLE keyv ENABLE ROW LEVEL SECURITY;
+  `)
   db.execute(sql`ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM anon;`)
 }
